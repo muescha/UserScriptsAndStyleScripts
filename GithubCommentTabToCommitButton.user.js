d0704c2876916ff3c9bd0207753301a2bab67053
Michael <github.com@nietzold.com>
2017-02-01 05:03:35 +0100

    add gist namespace

--- GithubCommentTabToCommitButton.user.js
+++ GithubCommentTabToCommitButton.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Github: CommentSkipFields
 // @namespace    https://github.com/muescha/UserScriptsAndStyleScripts
-// @version      0.2
-// @description  skip select attachmend and skip html markup help to get comment on TAB
+// @version      0.3
+// @description  skip select attachmend and skip html markup help to get comment on TAB - available in Sections: Issues, PullRequest,Gist
 // @author       Muescha
 // @match        https://github.com/*/*/issues/*
 // @match        https://github.com/*/*/pull/*
+// @match        https://gist.github.com/*
 // @grant        none
 // ==/UserScript==
 
