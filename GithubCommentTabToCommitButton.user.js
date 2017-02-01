142a301ecb46d6e6a493f5d38a62f723f5ea1749
Michael <github.com@nietzold.com>
2017-02-01 04:43:58 +0100

    update homepage

--- GithubCommentTabToCommitButton.user.js
+++ GithubCommentTabToCommitButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github: CommentSkipFields
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/muescha/UserScriptsAndStyleScripts
+// @version      0.2
 // @description  skip select attachmend and skip html markup help to get comment on TAB
 // @author       Muescha
 // @match        https://github.com/*/*/issues/*
